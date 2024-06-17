dfce53cd7d5d854dacc55fbb0418429fa3750230
Adam Peters <70253021+adam-peters@users.noreply.github.com>
2024-06-17 11:59:09 -0700

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,36 @@
 const removeFromArray = function() {
+
+    // The quantity of arguments is unknown
+    // so we store them in an array
+    const args = Array.from(arguments);
+
+    if(args.length === 0) {
+        return "ERROR";
+    }
+
+    if(args.length === 1) {
+        return args[0];
+    }
+
+    // Seperating the first argument from the rest
+    // which makes args become a list of numbers 
+    // we want removed from this array
+    let originalArray = args.shift(); 
+
+    // Create a return array
+    let finalArray = [];
+
+    // Only add the number if it isn't in the args list
+    // Array.indexOf == -1, means the item was not found
+    // for in loop returns the index as num
+    for(num in originalArray) {
+        if(args.indexOf(originalArray[num]) == -1) {
+            finalArray.push(originalArray[num]);
+        }
+    }
+
+    return finalArray;
+
 };
 
 // Do not edit below this line
