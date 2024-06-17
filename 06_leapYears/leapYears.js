2a074512850cd58db4e1663edacf7d12ce12cf27
Adam Peters <70253021+adam-peters@users.noreply.github.com>
2024-06-17 12:27:39 -0700

    Complete Execise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    // year % 4 is true and year % 100 is not true, return true
+    // or year % 4 is true and year % 100 is true and year % 400 is true
+    if((year % 4 === 0 && year % 100 !== 0) ||  
+        (year % 4 === 0 && year % 100 === 0 && year % 400 === 0)) {
+            return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
