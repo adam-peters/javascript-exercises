d4659875ce0c57c7e19bceff79b8beeb1f3928fc
Adam Peters <70253021+adam-peters@users.noreply.github.com>
2024-06-17 12:38:38 -0700

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInF) {
+  return parseFloat(((degreesInF - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInC) {
+  return parseFloat((degreesInC * (9/5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
